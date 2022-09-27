a7f99dbc0e80e6b2733c7a135e8076d6259888e3
Angelina Julia <ajuliammelo@gmail.com>
2022-09-27 20:32:31 -0300

    Add js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let Advice = document.getElementById('advice-texto')
+let id = document.getElementById('advice-numero')
+let button = document.getElementById('advice-botao')
+
+var dados 
+
+fetch('https://api.adviceslip.com/advice')
+.then(response=> response.json())
+.then(function(data){
+    dados = data
+    console.log(dados.slip)
+   
+    
+
+})
+.catch(function(){
+
+})
+
+button.addEventListener('click', (callback)=>{
+    Advice.innerHTML = `"${dados.slip.advice}"`
+    id.innerHTML = dados.slip.id
+    
+    callback(dados)
+
+})
+
+
+
+
